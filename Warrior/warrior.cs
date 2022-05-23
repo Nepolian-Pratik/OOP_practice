5a801f4ce86d0f9932ac611033e264e34df3d97c
Nepolian-Pratik <44637259+Nepolian-Pratik@users.noreply.github.com>
2022-05-23 22:19:31 +0545

    first commit

---
+++ Warrior/warrior.cs
@@ -0,0 +1,32 @@
+namespace Warrior
+{
+    class warrior
+    {
+        private int _health;
+        private int _attack;
+        private int _defence;
+        private string _name;
+        private bool _isAlive;
+        /*private int _speed;
+        private int _luck;
+        private int _level;
+        private int _exp;
+        private int _gold;
+        private int _maxHealth;
+        private int _maxAttack;
+        private int _maxDefence;
+        private int _maxSpeed;
+        private int _maxLuck;
+        private int _maxLevel;
+        private int _maxExp;
+        private int _maxGold;
+        private int _minHealth;
+        private int _minAttack;
+        private int _minDefence;
+        private int _minSpeed;
+        private int _minLuck;
+        private int _minLevel;
+        private int _minExp;
+        private int _minGold;*/
+    }
+}
